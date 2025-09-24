5719905a00eaee11f1d7e171b2bd1691259675c4
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-09-24 15:36:28 +0300

    add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import userRouter from './user/index';
 import adminRouter from './admin/index';
+
+
+
 const route = Router();
 
 route.use('/admin', adminRouter);
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 import authRouter from "../user/auth/auth";
 import categoryRouter from "../user/categoryRoutes";
 import brandRouter from "../user/brandRoutes";
+import productRouter from '../user/productRoutes';
+import cartRouter from '../user/cartRoutes';
+import orderRouter from '../user/orderRoutes';
+import wishlistRouter from '../user/wishlistRoutes'
 
 const route = Router();
 
@@ -9,6 +13,9 @@ const route = Router();
 route.use("/auth", authRouter);
 route.use("/category", categoryRouter);
 route.use("/brand", brandRouter);
-
+route.use("/product", productRouter);
+route.use("/cart", cartRouter);
+route.use("/order", orderRouter);
+route.use("/wishlist", wishlistRouter)
 
 export default route;
